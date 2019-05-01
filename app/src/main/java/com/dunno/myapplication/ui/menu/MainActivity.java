48f5246985754c67ffe5736667a3878134327e3c
Anthony <f>
2019-05-01 13:16:52 +0200

    +Login & Register

--- app/src/main/java/com/dunno/myapplication/ui/menu/MainActivity.java
+++ app/src/main/java/com/dunno/myapplication/ui/menu/MainActivity.java
@@ -1,12 +1,8 @@
-package com.dunno.myapplication;
+package com.dunno.myapplication.ui.menu;
 
-import android.app.Activity;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.constraint.ConstraintLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.MenuItem;
@@ -17,7 +13,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
-import com.dunno.myapplication.ui.login.LoginActivity;
+import com.dunno.myapplication.R;
+import com.dunno.myapplication.ui.loginregister.LoginActivity;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
